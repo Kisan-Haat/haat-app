c3131b241ad89be8bcdd08a5388202b25e83cce4
Ashish Suhane <ashishsuhane@hotwaxsystems.com>
2019-12-28 12:27:09 +0530

    Imporved: customer feeds html structure and css

--- app/containers/customer/customerFeed.js
+++ app/containers/customer/customerFeed.js
@@ -21,19 +21,137 @@ import Currency from "../../components/currency.component";
 import ApiHelper from "../../utils/api.helper";
 import { ADD_TO_CART } from "../../constants/ActionTypes";
 
+export default class Feeds extends Component {
+  _handleLoadMore = () => {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: null,
+      isLoading: true,
+      list: [],
+      cart: [],
+      data: [],
+      page: 1,
+      loading: true,
+      loadingMore: false,
+      error: null,
+      text: ""
+    };
+    this.getCartList();
+  }
+
+  static navigationOptions = {};
+
+  getCartList = () => {
+    ApiHelper.getPublishedProducts().then(res => {
+      this.setState({
+        list: res.data.data
+      });
+    });
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  addToCart(item, text) {
+    list.push(item, text);
+  }
+
+  render() {
+    if (this.state.list.length == undefined || this.state.list.length < 1) {
+      return (
+        <View paddingTop={60}>
+          <ActivityIndicator size="large" color="#6202EE" />
+          <Text style={styles.textHeader}>Loading Your Feed</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={{margin: 8}}>
+          <FlatList
+            data={this.state.list}
+            renderItem={({ item, index }) => (
+              <View style={styles.item}>
+                <Card style={styles.card}>
+                  <Card.Cover
+                    source={{ uri: item.products.image }}
+                    style={{ width:'100%', height: 300, alignSelf: "center" }}
+                  />
+                  <Card.Title
+                    title={item.products.hindiName}
+                    subtitle={item.products.englishName}
+                    right={props => (
+                      <Text style={{ marginRight: 25 }}>
+                        {item.products.price}₹ per {item.products.units}
+                      </Text>
+                    )}
+                  />
+                  <Card.Actions>
+                    <Button
+                      icon="account-box"
+                      style={{ size: 52 }}
+                      uppercase={false}
+                      labelStyle={{ color: "rgba(0, 0, 0, 0.87)" }}
+                      color="#757575"
+                      font="Roboto"
+                    >
+                      <Text>{item.partyName}</Text>
+                    </Button>
+                  </Card.Actions>
+                  <Divider inset />
+                  <Card.Actions>
+                    <TextInput
+                      style={{ width: 213, height: 56, marginRight: 20 }}
+                      mode="outlined"
+                      textAlignVertical="center"
+                      label="Quantity"
+                      onChangeText={text => {
+                        if (
+                          parseInt(text) <= item.products.quantity ||
+                          text == ""
+                        ) {
+                          this.setState({ text });
+                        } else {
+                          alert("invalid quantity");
+                          this.setState({ text: "" })
+                        }
+                      }}
+                      keyboardType={"number-pad"}
+                    />
+                    <Text style={styles.text}>
+                      Remaining {'\n'} { item.products.quantity}
+                    </Text>
+                  </Card.Actions>
+                  <Card.Content>
+                    <Text style={styles.subtext}>{item.products.units}</Text>
+                  </Card.Content>
+                  <Card.Actions>
+                    <Button
+                      color="#6202EE"
+                      onPress={() => this.addToCart(item, text.toString())}
+                    >
+                      Add to cart
+                    </Button>
+                  </Card.Actions>
+                </Card>
+              </View>
+            )}
+            keyExtractor={this.keyExtractor}
+          />
+        </View>
+      );
+    }
+  }
+}
+
 const styles = StyleSheet.create({
   item: {
     flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "stretch",
-    paddingTop: "2%",
-    paddingLeft: "2%",
-    paddingRight: "2%",
-    borderColor: "#ffffff",
-    borderWidth: 3,
-    backgroundColor: "#ffffff"
+    margin: 8
   },
   card: {
+    padding: 8,
     elevation: 8,
     backgroundColor: "#FAFAFA"
   },
@@ -61,139 +179,3 @@ const styles = StyleSheet.create({
     textAlign: "center"
   }
 });
-export default class Feeds extends Component {
-  _handleLoadMore = () => {};
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: null,
-      isLoading: true,
-      list: [],
-      cart: [],
-      data: [],
-      page: 1,
-      loading: true,
-      loadingMore: false,
-      error: null,
-      text: ""
-    };
-    this.getCartList();
-  }
-
-  static navigationOptions = {};
-
-  getCartList = () => {
-    ApiHelper.getPublishedProducts().then(res => {
-      this.setState({
-        list: res.data.data
-      });
-
-      //alert("data ===== " + JSON.stringify(res.data.data));
-    });
-  };
-
-  keyExtractor = (item, index) => index.toString();
-
-  addToCart(item, text) {
-    list.push(item, text);
-    alert(list);
-    /* let updateCart = this.state.cart || [];
-    updateCart.push(item);
-    this.setState({ cart: updateCart });
-    alert("add to cart" + JSON.stringify(this.state.cart));
-
-    dispatch.addToCart({
-      type: ADD_TO_CART,
-      payload: this.cart
-    });  */
-  }
-
-  render() {
-    if (this.state.list.length == undefined || this.state.list.length < 1) {
-      return (
-        <View paddingTop={60}>
-          <ActivityIndicator size="large" color="#6202EE" />
-          <Text style={styles.textHeader}>Loading Your Feed</Text>
-        </View>
-      );
-    } else {
-      return (
-        <View>
-          <FlatList
-            contentContainerStyle={{ paddingBottom: 55 }}
-            data={this.state.list}
-            renderItem={({ item, index }) => (
-              <View style={styles.item}>
-                <Card style={styles.card}>
-                  <Card.Cover
-                    source={{ uri: item.products.image }}
-                    style={{ height: 300, width: 300, alignSelf: "center" }}
-                  />
-                  <Card.Title
-                    title={item.products.hindiName}
-                    subtitle={item.products.englishName}
-                    right={props => (
-                      <Text style={{ marginRight: 25 }}>
-                        {item.products.price}₹ per {item.products.units}
-                      </Text>
-                    )}
-                  />
-                  <Card.Actions>
-                    <Button
-                      icon="account-box"
-                      style={{ size: 52 }}
-                      uppercase={false}
-                      labelStyle={{ color: "rgba(0, 0, 0, 0.87)" }}
-                      color="#757575"
-                      font="Roboto"
-                    >
-                      <Text>{item.partyName}</Text>
-                    </Button>
-                  </Card.Actions>
-                  <Divider inset />
-                  <Card.Actions>
-                    <TextInput
-                      style={{ width: 213, height: 56, marginRight: 60 }}
-                      mode="outlined"
-                      textAlignVertical="center"
-                      label="Quantity"
-                      onChangeText={text => {
-                        if (
-                          parseInt(text) <= item.products.quantity ||
-                          text == ""
-                        ) {
-                          this.setState({ text });
-                        } else {
-                          alert("invalid quantity");
-                          this.setState({ text: "" })
-                        }
-                      }}
-                      keyboardType={"number-pad"}
-                    />
-                    <Text style={styles.text}>
-                      Remaining: {"\n"}
-                      {item.products.quantity}
-                    </Text>
-                  </Card.Actions>
-                  <Card.Content>
-                    <Text style={styles.subtext}>{item.products.units}</Text>
-                  </Card.Content>
-                  <Card.Actions>
-                    <Button
-                      color="#6202EE"
-                      onPress={() => this.addToCart(item, text.toString())}
-                    >
-                      Add to cart
-                    </Button>
-                  </Card.Actions>
-                </Card>
-              </View>
-            )}
-            keyExtractor={this.keyExtractor}
-          />
-        </View>
-      );
-    }
-  }
-}
